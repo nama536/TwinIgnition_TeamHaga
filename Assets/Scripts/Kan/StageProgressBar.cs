1700376a0ae8e6d0fec2e23a69ba9e16a72993e7
mineko <ttc2550sg0023@edu.tech.ac.jp>
2026-07-11 16:39:22 +0900

    progressとHP

--- Assets/Scripts/Kan/StageProgressBar.cs
+++ Assets/Scripts/Kan/StageProgressBar.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class StageProgressBar : MonoBehaviour
 {
     public float speed = 0.1f; // Speed of the progress bar
     [SerializeField] private GameObject rocket;
+    [SerializeField] private GameObject target;
+    private bool arrived = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +16,23 @@ public class StageProgressBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rocket.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if (arrived) return;
+
+        rocket.transform.position = Vector3.MoveTowards(
+            rocket.transform.position,
+            target.transform.position,
+            speed * Time.deltaTime);
+
+        if (target.transform.position.y - rocket.transform.position.y < 0.01f)
+        {
+            arrived = true;
+            Debug.Log("“ž’…I");
+        }
+
+        if (target.transform.position.y - rocket.transform.position.y < 1f)
+        {
+            arrived = true;
+            Debug.Log("Boss");
+        }
     }
 }
